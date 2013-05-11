420cfcdbf4f51900c55ac9c9e6022d0062b659c0
Paul Le Cam <paul@dokiwi.com>
2013-05-11 22:24:34 +0200

    Generated documentation for ba1fb19a91e5c9a36c19e1e24d6bcb107e28d349

--- assets/behavior.js
+++ assets/behavior.js
@@ -30,6 +30,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 2,
+                "title": "Installation",
+                "slug": "installation"
+              },
+              "depth": 2
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
                 "title": "Previous work",
                 "slug": "previous-work"
               },
@@ -66,6 +74,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       "depth": 1,
       "outline": [
         {
+          "type": "heading",
+          "data": {
+            "level": 2,
+            "title": "Installation",
+            "slug": "installation"
+          },
+          "depth": 2
+        }, {
           "type": "heading",
           "data": {
             "level": 2,
